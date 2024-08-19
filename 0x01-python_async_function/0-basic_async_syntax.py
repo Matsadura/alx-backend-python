7a865be8a77067eacc336bba2be2aabe9eefb40f
Matsadura <zidanditoo1@gmail.com>
2024-08-19 13:59:34 +0100

    [Update] Change Type annotation, add await asynchio.sleep

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -2,11 +2,11 @@
 """ Contains an asynchronous coroutine """
 import asyncio
 import random
-from typing import Awaitable, Any
 
 
-async def wait_random(max_delay: float = 10) -> float:
+async def wait_random(max_delay: int = 10) -> float:
     """ Takes an argument, waits for a random relay
         and returns it eventually"""
-    num = random.uniform(0, max_delay)
-    return num
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
